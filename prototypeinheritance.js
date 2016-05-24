867fdddaf56413cb4c9988e60b5f287f73b4c292
AnthStep <lightlife25@gmail.com>
2016-05-24 13:25:15 +0300

    Signed-off-by: AnthStep <lightlife25@gmail.com>

---
+++ prototypeinheritance.js
@@ -0,0 +1,23 @@
+var Animal = function(age,name,sound,region){
+  this.age = age;
+  this.name = name;
+  this.sound = sound;
+  this.region = region;
+  this.say = function(){console.log(this.name+" say's "+this.sound)};
+}
+
+function inheritance(){
+Animal.apply(this,arguments);
+this.prototype = Animal;
+this.goAway = function(){console.log(this.name+" go away")};
+}
+
+var Dog = function(){ inherit.apply(this,arguments) };
+var Cat = function(){ inherit.apply(this,arguments) };
+var Woodpecker = function(){ inherit.apply(this,arguments) };
+
+
+
+
+tom = new Cat(1,'dicj','ddsf','fef');
+tom.say();
