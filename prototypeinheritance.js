9e30be70ca096cba4f971e8433a6609424a58326
AnthStep <lightlife25@gmail.com>
2016-05-25 13:05:34 +0300

    Signed-off-by: AnthStep <lightlife25@gmail.com>

--- prototypeinheritance.js
+++ prototypeinheritance.js
@@ -1,27 +1,72 @@
 var Animal = function(age,name,sound,region){
-  this.age = age;
-  this.name = name;
-  this.sound = sound;
-  this.region = region;
-  this.say = function(){console.log(this.name+" say's "+this.sound)};
+	 this.age = age;
+	 this.name = name;
+	 this.sound = sound;
+	 this.region = region;
 }
 
-function inheritance(){
-Animal.apply(this,arguments);
-this.prototype = Animal;
-this.goAway = function(){console.log(this.name+" go away")};
+Animal.prototype.say = function(){console.log(this.name+" say's "+this.sound)};
+
+function inheritanceProp(){
+	Animal.apply(this,arguments);
+	Object.defineProperty(this,"sound",{writable: false});
 }
 
-var Dog = function(){ inherit.apply(this,arguments) };
-var Cat = function(){ inherit.apply(this,arguments) };
-var Woodpecker = function(){ inherit.apply(this,arguments) };
-
-function getTypep() {
-	
+function inheritanceMethods(obj){
+	obj.prototype = new Animal();
+	obj.prototype.goAway = function(){console.log(this.name+" go away")};
 }
 
+var Dog = function(age,name,weight,region){ 
+	inheritanceProp.call(this,age,name,"Bark",region); 
+	this.weight=weight;
+};
+inheritanceMethods(Dog);
+
+var Cat = function(age,name,color,region){ 
+	inheritanceProp.call(this,age,name,"Meow",region);
+	this.color=color;
+};
+inheritanceMethods(Cat);
+
+var Woodpecker = function(age,name,kind,region){ 
+	inheritanceProp.call(this,age,name,"Guess Who?",region);
+	this.kind=kind;
+};
+inheritanceMethods(Woodpecker);
+
+function getTypep(obj) {
+	if ("weight" in obj){
+		return "Dog";
+	} else if("color" in obj){
+		return "Cat";
+	} else if("kind" in obj){
+		return "Woodpecker";
+	} else {
+		return "not an animal";
+	}
+};
+
+function getTypep2() {
+	obj = this;
+	if ("weight" in obj){
+		return "Dog";
+	} else if("color" in obj){
+		return "Cat";
+	} else if("kind" in obj){
+		return "Woodpecker";
+	} else {
+		return "not an animal";
+	}
+};
 
 
 
-tom = new Cat(1,'dicj','ddsf','fef');
-tom.say();
+
+dg = new Dog(102, "Goofy", "42kg", "Disney");
+ct = new Cat(30, "Thomas", "Gray/Blue", "Metro-Goldwyn-Mayer");
+wp = new Woodpecker(76, "Woody", "Pileated", "Universal Pictures");
+
+dg.say();
+ct.say();
+wp.say();
